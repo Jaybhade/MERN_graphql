e86009d9656d7b2e7e18ec2ad6b3d446d002d347
jaybhade <jayeshjibhade@gmail.com>
2020-08-21 23:42:38 +0530

    Added AuthorSchema and modified BookSchema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,14 +4,21 @@ const _ = require('lodash')
 const { GraphQLObjectType,
 	GraphQLString, 
 	GraphQLSchema,
-	GraphQLID 
+	GraphQLID,
+	GraphQLInt,
 } = graphql
 
 // dummy data
 var books = [
-	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-	{ name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+	{ name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+]
+
+var authors = [
+	{ name: 'Patrik Rothfuss', age: 44, id: '1' },
+	{ name: 'Brandon Sanderson', age: 42, id: '2' },
+	{ name: 'Terry Pratchett', age: 66, id: '3' }
 ]
 
 //book schema
@@ -20,7 +27,23 @@ const BookType = new GraphQLObjectType({
 	fields: () => ({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
-		genre: { type: GraphQLString }
+		genre: { type: GraphQLString },
+		author: {
+			type: AuthorType,
+			resolve(parent, args){
+				return _.find(authors, { id: parent.authorId })
+			}
+		}
+	})
+})
+
+//author schema
+const AuthorType = new GraphQLObjectType({
+	name: "Author",
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt }
 	})
 })
 
@@ -35,6 +58,13 @@ const RootQuery = new GraphQLObjectType({
 			resolve(parent, args){
 				//code to get data from db / other sources
 				return _.find(books, { id: args.id })
+			}
+		},
+		author: {
+			type: AuthorType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args){
+				return _.find(authors, { id: args.id })
 			}
 		}
 	}
