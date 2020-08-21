f4deee46692e1aa7c6c93dd674800fe684ff201a
jaybhade <jayeshjibhade@gmail.com>
2020-08-21 23:20:36 +0530

    Testing queries with GraphiQL, added dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,24 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
+const { GraphQLObjectType,
+	GraphQLString, 
+	GraphQLSchema,
+	GraphQLID 
+} = graphql
+
+// dummy data
+var books = [
+	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+	{ name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+]
 
 //book schema
 const BookType = new GraphQLObjectType({
 	name: "Book",
 	fields: () => ({
-		id: { type: GraphQLString },
+		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString }
 	})
@@ -19,9 +31,10 @@ const RootQuery = new GraphQLObjectType({
 	fields: {
 		book: {
 			type: BookType,
-			args: { id: { type: GraphQLString } }, //id to find book
+			args: { id: { type: GraphQLID } }, //id to find book
 			resolve(parent, args){
 				//code to get data from db / other sources
+				return _.find(books, { id: args.id })
 			}
 		}
 	}
