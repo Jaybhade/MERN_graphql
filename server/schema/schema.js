09e304f417c62a91c9c654f49434c94a99a1b7e7
jaybhade <jayeshjibhade@gmail.com>
2020-08-22 18:16:00 +0530

    Added GraphQLNonNull

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,7 @@ const { GraphQLObjectType,
 	GraphQLID,
 	GraphQLInt,
 	GraphQLList,
+	GraphQLNonNull,
 } = graphql
 
 // dummy data
@@ -109,8 +110,8 @@ const Mutation = new GraphQLObjectType({
 		addAuthor: {
 			type: AuthorType,
 			args: {
-				name: { type: GraphQLString },
-				age: { type: GraphQLInt }
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) }
 			},
 			resolve(parent, args){
 				let author = new Author({
@@ -123,9 +124,9 @@ const Mutation = new GraphQLObjectType({
 		addBook: {
 			type: BookType,
 			args: {
-				name: { type: GraphQLString },
-				genre: { type: GraphQLString },
-				authorId: { type: GraphQLID },
+				name: { type: new GraphQLNonNull(GraphQLString) },
+				genre: { type: new GraphQLNonNull(GraphQLString) },
+				authorId: { type: new GraphQLNonNull(GraphQLID) },
 			},
 			resolve(parent, args){
 				let book = new Book({
